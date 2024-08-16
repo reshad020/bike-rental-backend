61a86ec221ac4afe900c510b23d70affbd90037a
reshad020 <aehtesham2002@gmail.com>
2024-08-17 02:57:33 +0600

    remove unnecessary routes

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import { StudentRoutes } from "../modules/student/student.route";
 import { authRoutes } from "../modules/auth/auth.route";
 import { userRoutes } from "../modules/user/user.route";
 import { bikeRoutes } from "../modules/bike/bike.route";
@@ -7,7 +6,6 @@ import { bookingRoutes } from "../modules/booking/booking.route";
 
 const router = express.Router();
 
-router.use("/students", StudentRoutes);
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 router.use("/bikes", bikeRoutes);
